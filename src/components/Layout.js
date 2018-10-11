eaa404539d672014506a9b1f1ff97b18d3fdcc9e
Babbins <alimahmo@buffalo.edu>
2018-10-11 11:09:48 -0400

    Add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import './all.sass'
+import favicon from '../../static/favicon.ico'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Hannah Frishberg" />
+    <Helmet title="Hannah Frishberg"
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+    ]}/>
     <div>{children}</div>
   </div>
 )
